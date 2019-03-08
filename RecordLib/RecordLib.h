0315a35eaf057c3379687c5bc5d547480a6bc1ef
Ngoc Luong <luong.nn@seesaa.vn>
2019-03-08 17:21:34 +0700

    Init project setup

---
+++ RecordLib/RecordLib.h
@@ -0,0 +1,19 @@
+//
+//  RecordLib.h
+//  RecordLib
+//
+//  Created by Ngoc Luong on 3/8/19.
+//  Copyright © 2019 Seesaa Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RecordLib.
+FOUNDATION_EXPORT double RecordLibVersionNumber;
+
+//! Project version string for RecordLib.
+FOUNDATION_EXPORT const unsigned char RecordLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecordLib/PublicHeader.h>
+
+
---
+++ RecordLibSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecordLibSample
+//
+//  Created by Ngoc Luong on 3/8/19.
+//  Copyright © 2019 Seesaa Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
