ef2757291b35e10f69c2a403ac848895d4acaed6
Ngoc Luong <luong.nn@seesaa.vn>
2019-03-08 17:38:24 +0700

    Add test cases

---
+++ RecordLib/RecordLib.swift
@@ -0,0 +1,22 @@
+//
+//  RecordLib.swift
+//  RecordLib
+//
+//  Created by Ngoc Luong on 3/8/19.
+//  Copyright © 2019 Seesaa Inc. All rights reserved.
+//
+
+import Foundation
+public final class RecordLib {
+    
+    let name = "RecordLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- RecordLibTests/RecordLibTests.swift
+++ RecordLibTests/RecordLibTests.swift
@@ -10,25 +10,22 @@ import XCTest
 @testable import RecordLib
 
 class RecordLibTests: XCTestCase {
+    var recordLib: RecordLib!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        recordLib = RecordLib()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAdd() {
+        XCTAssertEqual(recordLib.add(a: 1, b: 2), 3)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(recordLib.sub(a: 2, b: 1), 1)
     }
 
 }
